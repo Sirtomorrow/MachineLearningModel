144ee9e2b930d9aa58f1fb2b3e5f9af83423bf7d
Ivan Yanez <ivanyanez7@yahoo.com>
2019-03-13 20:33:39 +0000

    Experimenting with files

--- TonsilitisMachineLearning/src/com/tonsilitis/assignment/Control.java
+++ TonsilitisMachineLearning/src/com/tonsilitis/assignment/Control.java
@@ -5,9 +5,16 @@ public class Control
 
 	public static void main(String[] args) 
 	{
-		GUI gui1 = new GUI("Tonsilitis Program");
-		System.out.println(gui1);
+		// get the data files
+		Files myFile = new Files();
+		System.out.println(myFile);
+		
+		// close the file
+		myFile.closeFile();
 
+		/* GUI gui1 = new GUI("Tonsilitis Program");
+		System.out.println(gui1); */
+		
 	}
 
 }
